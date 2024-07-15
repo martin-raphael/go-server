06050869bbd63bc2add5d26e2e0d812c4895a58e
martin-raphael <martinraphael254@gmail.com>
2024-07-15 10:40:25 +0300

    first server

---
+++ .history/server_20240715101821.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", GetUsers).Methods("GET")
+	router.HandleFunc("/users", CreateUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
+}
